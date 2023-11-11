83abc710960005558d594df84a3daf26773d40ce
Sanjeev <sanjeev.sritharan007@gmail.com>
2023-11-11 15:06:08 -0500

    made changes so that template works with wsl

--- peer-sk.c
+++ peer-sk.c
@@ -1,244 +1,223 @@
 /* A P2P client
 It provides the following functions:
 - Register the content file to the index server (R)
-- Contact the index server to search for a content file (D) 
-	- Contact the peer to download the file
-	- Register the content file to the index server
+- Contact the index server to search for a content file (D)
+        - Contact the peer to download the file
+        - Register the content file to the index server
 - De-register a content file (T)
 - List the local registered content files (L)
 - List the on-line registered content files (O)
 */
-
-#include <stdio.h>
+#define _XOPEN_SOURCE 700
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <netdb.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <signal.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <signal.h>
 #include <sys/select.h>
-#include <errno.h>
-#include <arpa/inet.h>
-
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define QUIT "quit"
-#define SERVER_PORT 10000	/* well-known port */
-#define BUFLEN	100		/* buffer length */
+#define SERVER_PORT 10000 /* well-known port */
+#define BUFLEN 100        /* buffer length */
 #define NAMESIZ 20
-#define MAXCON	200
+#define MAXCON 200
 
-typedef struct 
-{
-char 	type;
-char	data[BUFLEN];
-} PDU;	
-PDU	rpdu;
+#define h_addr h_addr_list[0]
+
+typedef struct {
+    char type;
+    char data[BUFLEN];
+} PDU;
+PDU rpdu;
 
 struct {
-	int	val;
-	char 	name[NAMESIZ];
-} table[MAXCON];  //Keep Track of the registered content
+    int val;
+    char name[NAMESIZ];
+} table[MAXCON];  // Keep Track of the registered content
 
 char usr[NAMESIZ];
 
-int s_sock,peer_port;
+int s_sock, peer_port;
 int fd, nfds;
 fd_set rfds, afds;
 
-void 	registration(int, char *); 
-int 	search_content(int, char *, PDU *);
-int 	client_download(char *, PDU *);
-void 	server_download();
-void 	deregistration(int, char *);
-void	online_list(int);
-void	local_list();
-void	quit(int);
-void	handler();
+void registration(int, char *);
+int search_content(int, char *, PDU *);
+int client_download(char *, PDU *);
+void server_download();
+void deregistration(int, char *);
+void online_list(int);
+void local_list();
+void quit(int);
+void handler();
 
-int main(int argc, char **argv)
-{
-	int	s_port = SERVER_PORT;
-	int 	n;
-	int	alen = sizeof(struct sockaddr_in);
-	struct	hostent		*hp;
-	struct	sockaddr_in server;
-	char	c, *host, name[NAMESIZ];
-	struct	sigaction sa;
+int main(int argc, char **argv) {
+    int s_port = SERVER_PORT;
+    int n;
+    int alen = sizeof(struct sockaddr_in);
+    struct hostent *hp;
+    struct sockaddr_in server;
+    char c, *host, name[NAMESIZ];
+    struct sigaction sa;
 
-	switch(argc){
-		case 2:
-	  		host = argv[1];
-	  		break;
-		case 3:
-  	  		host = argv[1];
-	  		s_port = atoi(argv[2]);
-			break;
-		default:
-	  		printf("Usage: %s host [port]\n", argv[0]);
-	  		exit(1);
-	}
+    switch (argc) {
+        case 2:
+            host = argv[1];
+            break;
+        case 3:
+            host = argv[1];
+            s_port = atoi(argv[2]);
+            break;
+        default:
+            printf("Usage: %s host [port]\n", argv[0]);
+            exit(1);
+    }
 
-  /* UDP Connection with the index server		*/ 
-	memset(&server, 0, alen);
-	server.sin_family = AF_INET;
-	server.sin_port = htons(s_port);
-	if ( hp = gethostbyname(host) )
-		memcpy(&server.sin_addr, hp->h_addr, hp->h_length);
-	else if ( (server.sin_addr.s_addr = inet_addr(host)) == INADDR_NONE ){
-        	printf("Can't get host entry \n");
-    		exit(1);
-	}
-	s_sock = socket(PF_INET, SOCK_DGRAM, 0); // Allocate a socket for the index server
-	if (s_sock < 0){
-    		printf("Can't create socket \n");
-    		exit(1);
-	} 
-	if (connect(s_sock, (struct sockaddr *)&server, sizeof(server)) < 0){
-		printf("Can't connect \n");
-		exit(1);
-	}
+    /* UDP Connection with the index server		*/
+    memset(&server, 0, alen);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(s_port);
+    if (hp = gethostbyname(host))
+        memcpy(&server.sin_addr, hp->h_addr, hp->h_length);
+    else if ((server.sin_addr.s_addr = inet_addr(host)) == INADDR_NONE) {
+        printf("Can't get host entry \n");
+        exit(1);
+    }
+    s_sock = socket(PF_INET, SOCK_DGRAM,
+                    0);  // Allocate a socket for the index server
+    if (s_sock < 0) {
+        printf("Can't create socket \n");
+        exit(1);
+    }
+    if (connect(s_sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        printf("Can't connect \n");
+        exit(1);
+    }
 
-/* 	Enter User Name		*/
-	printf("Choose a user name\n");
-	scanf("%s",usr);
+    /* 	Enter User Name		*/
+    printf("Choose a user name\n");
+    scanf("%s", usr);
 
-/* Initialization of SELECT`structure and table structure	*/
-	FD_ZERO(&afds);
-	FD_SET(s_sock, &afds);       /* Listening on the index server socket  */
-	FD_SET(0, &afds);       /* Listening on the read descriptor   */
-	nfds = 1;
-	for(n=0; n<MAXCON; n++)
-		table[n].val = -1;
+    /* Initialization of SELECT`structure and table structure	*/
+    FD_ZERO(&afds);
+    FD_SET(s_sock, &afds); /* Listening on the index server socket  */
+    FD_SET(0, &afds);      /* Listening on the read descriptor   */
+    nfds = 1;
+    for (n = 0; n < MAXCON; n++) table[n].val = -1;
 
-/*	Setup signal handler		*/
-	sa.sa_handler = handler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sigaction(SIGINT, &sa, NULL);
-                                                                                
-/* Main Loop	*/
-	while(1){
-	  printf("Command:\n");
+    /*	Setup signal handler		*/
+    sa.sa_handler = handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
 
-	  memcpy(&rfds, &afds, sizeof(rfds));
-    	  if (select(nfds, &rfds, NULL, NULL, NULL) == -1){
-       	    printf("select error: %s\n",strerror(errno));
-       	    exit(1);
-	  }
+    /* Main Loop	*/
+    while (1) {
+        printf("Command:\n");
 
-    	  if (FD_ISSET(0, &rfds)) {  /* Command from the user  */
-	    c = getchar();
+        memcpy(&rfds, &afds, sizeof(rfds));
+        if (select(nfds, &rfds, NULL, NULL, NULL) == -1) {
+            printf("select error: %s\n", strerror(errno));
+            exit(1);
+        }
 
-/*	Command options	*/
-	    if(c=='?'){
-		printf("R-Content Registration; T-Content Deregistration; L-List Local Content\n");
-       	 	printf("D-Download Content; O-List all the On-line Content; Q-Quit\n\n");
-		continue;
-	    }
+        if (FD_ISSET(0, &rfds)) { /* Command from the user  */
+            c = getchar();
 
-/*	Content Regisration	*/
-    	    if(c=='R'){
-		/* Call registration() */
-  	    }
+            /*	Command options	*/
+            if (c == '?') {
+                printf(
+                    "R-Content Registration; T-Content Deregistration; L-List "
+                    "Local Content\n");
+                printf(
+                    "D-Download Content; O-List all the On-line Content; "
+                    "Q-Quit\n\n");
+                continue;
+            }
 
-/*	List Content		*/
-	    if(c == 'L'){
-		/* Call local_list()	*/
-		}
+            /*	Content Regisration	*/
+            if (c == 'R') {
+                /* Call registration() */
+            }
 
-/*	List on-line Content	*/
-	    if(c == 'O'){
-		/* Call online_list()	*/
-		}
+            /*	List Content		*/
+            if (c == 'L') {
+                /* Call local_list()	*/
+            }
 
-/*	Download Content	*/
-  	    if(c=='D'){
-		/* Call search_content()	*/
-		/* Call client_download()	*/
-		/* Call registration()		*/
-	      }
+            /*	List on-line Content	*/
+            if (c == 'O') {
+                /* Call online_list()	*/
+            }
 
-/*	Content Deregistration	*/
-	    if(c == 'T'){
-		/* Call deregistration()	*/
-		}
+            /*	Download Content	*/
+            if (c == 'D') {
+                /* Call search_content()	*/
+                /* Call client_download()	*/
+                /* Call registration()		*/
+            }
 
-/*	Quit	*/
-	    if(c == 'Q'){
-		/* Call quit()	*/
-	  	}
-	    
-  	  }
+            /*	Content Deregistration	*/
+            if (c == 'T') {
+                /* Call deregistration()	*/
+            }
 
+            /*	Quit	*/
+            if (c == 'Q') {
+                /* Call quit()	*/
+            }
 
-/* Content transfer: Server to client		*/
-	  else
-	    server_download(s_sock);
-	
-	}
+        }
+
+        /* Content transfer: Server to client		*/
+        else
+            server_download(s_sock);
+    }
 }
 
-
-void	quit(int s_sock)
-{
-	/* De-register all the registrations in the index server	*/
+void quit(int s_sock) {
+    /* De-register all the registrations in the index server	*/
 }
 
-void	local_list()
-{
-	/* List local content	*/	
+void local_list() { /* List local content	*/
 }
 
-void	online_list(int s_sock)
-{
-	/* Contact index server to acquire the list of content */
+void online_list(int s_sock) {
+    /* Contact index server to acquire the list of content */
 }
 
-
-
-void	server_download()
-{
-	/* Respond to the download request from a peer	*/
+void server_download() { /* Respond to the download request from a peer	*/
 }
 
-
-int search_content(int s_sock, char *name, PDU *rpdu)
-{
-	/* Contact index server to search for the content
-	   If the content is available, the index server will return
-	   the IP address and port number of the content server.	*/
-
-
+int search_content(int s_sock, char *name, PDU *rpdu) {
+    /* Contact index server to search for the content
+       If the content is available, the index server will return
+       the IP address and port number of the content server.	*/
 }
 
-int client_download(char *name, PDU *pdu)
-{
-	/* Make TCP connection with the content server to initiate the
-	   Download.	*/
-
+int client_download(char *name, PDU *pdu) {
+    /* Make TCP connection with the content server to initiate the
+       Download.	*/
 }
 
-void deregistration(int s_sock,char *name)
-{
- 	/* Contact the index server to deregister a content registration;	   Update nfds. */
-
+void deregistration(int s_sock, char *name) {
+    /* Contact the index server to deregister a content registration; Update
+     * nfds. */
 }
 
-
-void registration(int s_sock,char *name)
-{
-	/* Create a TCP socket for content download 
-			– one socket per content;
-	   Register the content to the index server;
-	   Update nfds;	*/
-
+void registration(int s_sock, char *name) {
+    /* Create a TCP socket for content download
+                    ï¿½ one socket per content;
+       Register the content to the index server;
+       Update nfds;	*/
 }
 
-void	handler()
-{
-	quit(s_sock);
-}
+void handler() { quit(s_sock); }
--- server-sk.c
+++ server-sk.c
@@ -1,4 +1,4 @@
-/* Index Server 
+/* Index Server
 
 Message types:
 R - used for registration
@@ -12,43 +12,43 @@ E - Error messages from the Server
 
 */
 
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #include <netinet/in.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <netdb.h>
-#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <time.h>
-#include <arpa/inet.h>
 
 #define MSG1 "Cannot find content"
-#define BUFLEN	100
-#define NAMESIZ	20
-#define MAX_NUM_CON	200
-                                                                                
-typedef struct entry{
-	char	usr[NAMESIZ];
-	struct sockaddr_in addr;
-	short	token;		
-	struct entry *next;
-} ENTRY; 
+#define BUFLEN 100
+#define NAMESIZ 20
+#define MAX_NUM_CON 200
 
-typedef struct{
-	char name[NAMESIZ];
-	ENTRY	*head;
+typedef struct entry {
+    char usr[NAMESIZ];
+    struct sockaddr_in addr;
+    short token;
+    struct entry *next;
+} ENTRY;
+
+typedef struct {
+    char name[NAMESIZ];
+    ENTRY *head;
 } LIST;
-LIST	list[MAX_NUM_CON];
-int	max_index=0;
+LIST list[MAX_NUM_CON];
+int max_index = 0;
 
-typedef struct{
-	char type;
-	char data[BUFLEN];
+typedef struct {
+    char type;
+    char data[BUFLEN];
 } PDU;
-PDU 	tpdu;
+PDU tpdu;
 
 void search(int, char *, struct sockaddr_in *);
-void registration(int, char *, struct sockaddr_in *); 
+void registration(int, char *, struct sockaddr_in *);
 void deregistration(int, char *, struct sockaddr_in *);
 
 /*
@@ -57,101 +57,93 @@ void deregistration(int, char *, struct sockaddr_in *);
  *------------------------------------------------------------------------
  */
 
-int
-main(int argc, char *argv[])
-{
-	struct sockaddr_in sin, *p_addr;	/* the from address of a client	*/
-	ENTRY	*p_entry;
-	char	*service = "10000";	/* service name or port number	*/
-	char	name[NAMESIZ], usr[NAMESIZ];
-	int	alen = sizeof(struct sockaddr_in);	/* from-address length		*/
-        int     s, n, i, len,p_sock;        /* socket descriptor and socket type    */
-	int	pdulen=sizeof(PDU);
-	struct  hostent         *hp;
-	PDU	rpdu;
-	struct sockaddr_in fsin;	/* the from address of a client	*/
+int main(int argc, char *argv[]) {
+    struct sockaddr_in sin, *p_addr; /* the from address of a client	*/
+    ENTRY *p_entry;
+    char *service = "10000"; /* service name or port number	*/
+    char name[NAMESIZ], usr[NAMESIZ];
+    int alen = sizeof(struct sockaddr_in); /* from-address length
+                                            */
+    int s, n, i, len, p_sock; /* socket descriptor and socket type    */
+    int pdulen = sizeof(PDU);
+    struct hostent *hp;
+    PDU rpdu;
+    struct sockaddr_in fsin; /* the from address of a client	*/
 
+    for (n = 0; n < MAX_NUM_CON; n++) list[n].head = NULL;
 
-	for(n=0; n<MAX_NUM_CON; n++)
-		list[n].head = NULL;
-                                                                                
-	switch (argc) {
-	case	1:
-		break;
-	case	2:
-		service = argv[1];
-		break;
-	default:
-		fprintf(stderr, "usage: chat_server [host [port]]\n");
+    switch (argc) {
+        case 1:
+            break;
+        case 2:
+            service = argv[1];
+            break;
+        default:
+            fprintf(stderr, "usage: chat_server [host [port]]\n");
+    }
 
-	}
-                                                                        
-        memset(&sin, 0, sizeof(sin));
-        sin.sin_family = AF_INET;
-        sin.sin_addr.s_addr = INADDR_ANY;
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
 
-   /* Map service name to port number */
-        sin.sin_port = htons((u_short)atoi(service));
-                                                                                
+    /* Map service name to port number */
+    sin.sin_port = htons((unsigned)atoi(service));
+
     /* Allocate a socket */
-        s = socket(AF_INET, SOCK_DGRAM, 0);
-        if (s < 0){
-		fprintf(stderr, "can't creat socket\n");
-		exit(1);
-	}
-                                                                                
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (s < 0) {
+        fprintf(stderr, "can't creat socket\n");
+        exit(1);
+    }
+
     /* Bind the socket */
-        if (bind(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
-		fprintf(stderr, "can't bind to %s port\n", service);
+    if (bind(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+        fprintf(stderr, "can't bind to %s port\n", service);
 
-	while (1) {
-		if ( (n=recvfrom(s, &rpdu, pdulen, 0,(struct sockaddr *)&fsin, &alen)) < 0)
-		  printf("recvfrom error: n=%d\n",n);
+    while (1) {
+        if ((n = recvfrom(s, &rpdu, pdulen, 0, (struct sockaddr *)&fsin,
+                          &alen)) < 0)
+            printf("recvfrom error: n=%d\n", n);
 
+        /*	Content Registration Request			*/
+        if (rpdu.type == 'R') {
+            /*	Call registration()
+             */
+        }
 
-	/*	Content Registration Request			*/
-		if(rpdu.type == 'R'){
-		  /*	Call registration()
-		  */		
-		}
+        /* Search Content		*/
+        if (rpdu.type == 'S') {
+            /* Call search()
+             */
+        }
 
-	/* Search Content		*/
-		if(rpdu.type == 'S'){ 
-		  /* Call search()
-		  */
-		}
+        /*	List current Content */
+        if (rpdu.type == 'O') {
+            /* Read from the content list and send the list to the
+               client 		*/
+        }
 
-	/*	List current Content */
-		if(rpdu.type == 'O'){
-		  /* Read from the content list and send the list to the 
-		     client 		*/
-
-		}
-			
-	/*	De-registration		*/
-		if(rpdu.type == 'T'){
-		  /* Call deregistration()
-		  */
-		}
-	}
-	return;
+        /*	De-registration		*/
+        if (rpdu.type == 'T') {
+            /* Call deregistration()
+             */
+        }
+    }
+    return;
 }
 
-void search(int s, char *data, struct sockaddr_in *addr)
-{
-	/* Search content list and return the answer:
-	   If found, send the address of the selected content server. 
-	*/
+void search(int s, char *data, struct sockaddr_in *addr) {
+    /* Search content list and return the answer:
+       If found, send the address of the selected content server.
+    */
 }
 
-void deregistration(int s, char *data, struct sockaddr_in *addr)
-{
-	/* De-register the server of that content 
-	*/
+void deregistration(int s, char *data, struct sockaddr_in *addr) {
+    /* De-register the server of that content
+     */
 }
 
-void registration(int s, char *data, struct sockaddr_in *addr)
-{
-	/* Register the content and the server of the content
-	*/
+void registration(int s, char *data, struct sockaddr_in *addr) {
+    /* Register the content and the server of the content
+     */
 }
